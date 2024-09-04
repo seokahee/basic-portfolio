4960c8594bbbed2d32ec2a21b338489acab841f3
서가희 <kahee430@gmail.com>
2024-09-04 23:00:31 +0900

    ✨ skill section 완료!

---
+++ index.js
@@ -0,0 +1,72 @@
+const toggleBtns = document.querySelectorAll(".toggleBtn");
+
+toggleBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const targetId = btn.getAttribute("data-target");
+    const targetSection = document.getElementById(targetId);
+
+    // 클릭한 버튼의 상태 확인
+    const isActive = btn.classList.contains("active");
+
+    // 모든 버튼에서 active 클래스 제거
+    toggleBtns.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    // 모든 섹션을 숨김
+    document.querySelectorAll(".section-content").forEach((section) => {
+      section.classList.remove("active");
+    });
+
+    if (!isActive) {
+      // 클릭한 버튼이 비활성 상태일 때만 active 클래스 추가
+      btn.classList.add("active");
+
+      // 클릭한 버튼의 타겟 섹션만 보이게 함
+      if (targetSection) {
+        targetSection.classList.add("active");
+      }
+    }
+  });
+});
+
+const frontendImages = [
+  { src: "/img/icons/next.png", alt: "Next.js" },
+  { src: "/img/icons/js.png", alt: "JavaScript" },
+  { src: "/img/icons/ts.png", alt: "Typescript" },
+  { src: "/img/icons/redux.png", alt: "Redux" },
+  { src: "/img/icons/zustand.png", alt: "Zustand" },
+  { src: "/img/icons/axios.png", alt: "Axios" },
+  { src: "/img/icons/query.png", alt: "TanStack-Query" },
+  { src: "/img/icons/tailwind.png", alt: "Tailwind css" },
+  { src: "/img/icons/style.png", alt: "Styled-components" },
+];
+
+const backendImages = [
+  { src: "/img/icons/Java.png", alt: "Java" },
+  { src: "/img/icons/cat.png", alt: "Apache Tomcat" },
+  { src: "/img/icons/oracle.png", alt: "Oracle" },
+  { src: "/img/icons/sql.png", alt: "SQL" },
+  { src: "/img/icons/my.png", alt: "MySQL" },
+];
+
+const frontendList = document.getElementById("frontend-list");
+const backendList = document.getElementById("backend-list");
+
+frontendImages.forEach((image) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = image.src;
+  img.alt = image.alt;
+  li.appendChild(img);
+  frontendList.appendChild(li);
+});
+
+backendImages.forEach((image) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = image.src;
+  img.alt = image.alt;
+  li.appendChild(img);
+  backendList.appendChild(li);
+});
